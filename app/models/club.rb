8c28bde1da9ce5089158bae6ca550bb9d786b9b6
Devon Noel de Tilly <devon.noeldetilly@gmail.com>
2017-06-19 12:16:00 -0400

    Add ownership to clubs

--- app/models/club.rb
+++ app/models/club.rb
@@ -1,2 +1,3 @@
 class Club < ActiveRecord::Base
+  belongs_to :user
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,5 @@
-Club.destroy_all
 User.destroy_all
-
-clubs = [
-  {name: "The Green Dragon", description: "The place where the coolest vertically challenged citizens of Middle-Earth get loud and tell tall tales"},
-  {name: "Cantina at Mos Eisley", description: "If you want to prove you can hang with scum and villainy's greatest hits, this is your joint"},
-  {name: "The Hog's Head", description: "Aspiring wizards drink butterbeer here and host mind-numbing after parties"},
-  {name: "The Babylon Club", description: "Say hello to your little friend, or other friends at this south Florida staple"}
-]
-
-clubs.each do |club|
-  Club.create(club)
-end
+Club.destroy_all
 
 users = [
   {name: "frodo", password: "1234", password_confirmation: "1234"},
@@ -21,4 +10,15 @@ users = [
 
 users.each do |user|
   User.create(user)
+end
+
+clubs = [
+  {name: "The Green Dragon", description: "The place where the coolest vertically challenged citizens of Middle-Earth get loud and tell tall tales", user: User.last},
+  {name: "Cantina at Mos Eisley", description: "If you want to prove you can hang with scum and villainy's greatest hits, this is your joint", user: User.last},
+  {name: "The Hog's Head", description: "Aspiring wizards drink butterbeer here and host mind-numbing after parties", user: User.last},
+  {name: "The Babylon Club", description: "Say hello to your little friend, or other friends at this south Florida staple", user: User.last}
+]
+
+clubs.each do |club|
+  Club.create(club)
 end
